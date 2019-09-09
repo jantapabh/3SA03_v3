f366de5a7fac7dd0f99893f3d0d166a50f8de3b6
jantapa2407 <jantapa2407@gmail.com>
2019-09-09 14:54:24 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import Content from './Content';
 import './App.css';
 import logo from './logo.svg'
 
- 
 let x = 10;
-class App extends Component {
+class App extends Component (){
  render() {
  return (
  <div className="App">
- 
+ <Content/>
  </div>
  );
  }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Content from './Content';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
