c24f2764eaf79284a635b1740a5c4a1595e8e678
jantapa <jantapa2407@gmail.com>
2019-09-09 15:35:26 +0700

    check 4 -5

--- src/Content.js
+++ src/Content.js
@@ -1,35 +1,22 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 
 export default class Content extends Component{
     constructor(){
-      super()
-      this.state={
-      
-   count:0,
-   name:' '
-
-      }
+        super()
+        this.state={
+            count:0
+        }
     }
+
     addNum=()=>{
-    
-        this.setState({
-            
- count: this.state.count+1,
- name:'test'
-
-        })
-
+        this.setState({count:this.state.count+1})
     }
 
-  render(){
-    return(
-   <div>
-   name: {this.state.name}
-   <button onClick={this.setState.addNum}>count: {this.state.count}</button>
-
-   </div>
-
-    );
-  }
-
+    render(){
+        return(
+            <div>
+                <button onClick={this.addNum}>num {this.state.count}</button>
+            </div>
+        )
+    }
 }
