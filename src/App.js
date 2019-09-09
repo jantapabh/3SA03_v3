d33ce81cc4f97c0609c8f8d7de6c24cf3db112ad
jantapa <jantapa2407@gmail.com>
2019-09-09 14:25:13 +0700

    Fisrt Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react';
+import Content from './Content';
+import './App.css';
+import logo from './logo.svg'
+
+ 
+let x = 10;
+class App extends Component {
+ render() {
+ return (
+ <div className="App">
+ 
+ </div>
+ );
+ }
+}
+export default App;
---
+++ src/Content.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'React';
+
+export default class Content extends Component{
+    constructor(){
+      super()
+      this.state={
+      
+   count:0,
+   name:' '
+
+      }
+    }
+    addNum=()=>{
+    
+        this.setState({
+            
+ count: this.state.count+1,
+ name:'test'
+
+        })
+
+    }
+
+  render(){
+    return(
+   <div>
+   name: {this.state.name}
+   <button onClick={this.setState.addNum}>count: {this.state.count}</button>
+
+   </div>
+
+    );
+  }
+
+}
