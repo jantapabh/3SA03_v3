c922e5c70bc0a8903ffb77cfd29b369d8dab9241
jantapa <jantapa2407@gmail.com>
2019-09-11 01:01:34 +0700

    git commit 16

--- src/App.js
+++ src/App.js
@@ -4,35 +4,78 @@ import './App.css';
 import _ from 'lodash'
 import style from './App.css'
 
-class App extends Component{
-
-  render(){
-
-    <div className="App">
-
-      <table className="Navbar">
-
-  <body>
-
-    <tr>
-      <td>
 
 
-        <img src={require('./src/logo.svg')} width="50" />
-          
-             </td>
-             <td>
+let message = 'HELLOJANJOA'
+const prepareStateFromWord = (given_word) => {
+  let word = given_word.toUpperCase()
+  let chars = _.shuffle(Array.from(word))
+  
+  return {
+  word,
+  chars,
+  attempt: 1,
+  guess: [],
+  completed: false
+  }
+ }
+
+class App extends React.Component {
+
+  state = prepareStateFromWord(message)
+
+  activationHandler = (c) => {
+    let guess = [...this.state.guess, c]
+    this.setState({guess})
+    if(guess.length == this.state.chars.length){
+    if(guess.join('').toString() == this.state.word){
+    this.setState({guess: [], completed: true})
+    }else{
+    this.setState({guess: [], attempt: this.state.attempt + 1})
+    }
+    }
+   }
+
+  render() {
+    return (
+      
+        
+      <div>
+        {
+          Array.from(this.state.chars).map((item, index) => (
+            <CharacterCard
+              value={item}
+              key={index}
+              activationHandler={this.activationHandler}
+            />
+          ))   
+      
+        }
 
 
-               CodeMobiles React.js
-             </td>
-    </tr>
-  </body>
-   
+        <h2 className="activeCard">Choose</h2>
+        {
+          Array.from(this.state.guess).map((item, index) => (
+            <CharacterCard
+              value={item}
+              key={index}
+              activationHandler={this.activationHandler}
+            />
+          ))
 
-      </table>
+        }
+        <div>Attemp {this.state.attempt}</div>
+        {
+            
+          this.state.completed && <h1>"Correct"</h1>
+        
+        }
+        
+      </div>
 
-</div>
 
+    )
   }
 }
+
+export default App;
